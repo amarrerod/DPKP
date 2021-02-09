825c1d378262c606443f409be86de78db2768de1
amarrerod <alemarrerodiaz@gmail.com>
2021-02-09 09:55:24 +0000

    fix: Printing results correctly

--- src/knapsackDP.cpp
+++ src/knapsackDP.cpp
@@ -10,47 +10,47 @@ const string KnapsackDP::EXTENSION = ".DP";
 KnapsackDP::KnapsackDP(const int& nItems, vector<p>& profits,
                        vector<w>& weights, const p& capacity)
     : maximumCapacity(capacity), numberOfItems(nItems) {
-  this->profits.reserve(nItems);
-  this->weights.reserve(nItems);
-  for (int i = 0; i < nItems; i++) {
-    this->profits[i] = profits[i];
-    this->weights[i] = weights[i];
-  }
-  instanceFile = INST_NONE;
+    this->profits.reserve(nItems);
+    this->weights.reserve(nItems);
+    for (int i = 0; i < nItems; i++) {
+        this->profits[i] = profits[i];
+        this->weights[i] = weights[i];
+    }
+    instanceFile = INST_NONE;
 }
 
 KnapsackDP::KnapsackDP(const string& filename, const fs::path& pathToResults)
     : maximumCapacity(0), numberOfItems(0) {
-  ifstream input(filename.c_str());
-  if (!input.is_open()) {
-    cerr << "Error while opening instance file: " << filename << endl;
-    exit(EXIT_FAILURE);
-  }
-  string readLine;
-  // Leemos la configuracion de la instance y adaptamos los vectores
-  input >> numberOfItems >> maximumCapacity;
-  profits.reserve(numberOfItems);
-  weights.reserve(numberOfItems);
-  table.resize(numberOfItems + 1, vector<int>(maximumCapacity + 1, 0.0));
+    ifstream input(filename.c_str());
+    if (!input.is_open()) {
+        cerr << "Error while opening instance file: " << filename << endl;
+        exit(EXIT_FAILURE);
+    }
+    string readLine;
+    // Leemos la configuracion de la instance y adaptamos los vectores
+    input >> numberOfItems >> maximumCapacity;
+    profits.reserve(numberOfItems);
+    weights.reserve(numberOfItems);
+    table.resize(numberOfItems + 1, vector<int>(maximumCapacity + 1, 0.0));
+    int w, p;
+    while (input >> w >> p) {
+        weights.push_back(w);
+        profits.push_back(p);
+    }
+    input.close();
+    fs::path instancePath = filename;
+    outputFile = (pathToResults / instancePath.filename().replace_extension(EXTENSION));
 #ifdef DEBUG
-  cout << "Filename: " << filename << endl;
-  cout << "Number of items: " << numberOfItems << endl;
-  cout << "Maximum Capacity: " << maximumCapacity << endl;
+    cout << "Filename: " << filename << endl;
+    cout << "Output file: " << outputFile << endl;
+    cout << "Number of items: " << numberOfItems << endl;
+    cout << "Maximum Capacity: " << maximumCapacity << endl;
 #endif
-  int w, p;
-  while (input >> w >> p) {
-    weights.push_back(w);
-    profits.push_back(p);
-  }
-  input.close();
-  fs::path filenamePath = filename;
-  instanceFile =
-      pathToResults / filenamePath.filename().replace_extension(EXTENSION);
 }
 
 KnapsackDP::~KnapsackDP() {
-  table.clear();
-  table.shrink_to_fit();
+    table.clear();
+    table.shrink_to_fit();
 }
 
 /**
@@ -59,44 +59,44 @@ KnapsackDP::~KnapsackDP() {
  *
  * */
 results KnapsackDP::run() {
-  clock_t startCPUTime = clock();
-  auto startWallTime = chrono::system_clock::now();
-  for (int i = 0; i <= numberOfItems; i++) {
-    for (int j = 0; j <= maximumCapacity; j++) {
-      if (i == 0 || j == 0) {
-        table[i][j] = 0;
-      } else {
-        if (weights[i - 1] <= j) {
-          table[i][j] = max(profits[i - 1] + table[i - 1][j - weights[i - 1]],
-                            table[i - 1][j]);
-        } else {
-          table[i][j] = table[i - 1][j];
+    clock_t startCPUTime = clock();
+    auto startWallTime = chrono::system_clock::now();
+    for (int i = 0; i <= numberOfItems; i++) {
+        for (int j = 0; j <= maximumCapacity; j++) {
+            if (i == 0 || j == 0) {
+                table[i][j] = 0;
+            } else {
+                if (weights[i - 1] <= j) {
+                    table[i][j] = max(profits[i - 1] + table[i - 1][j - weights[i - 1]],
+                                      table[i - 1][j]);
+                } else {
+                    table[i][j] = table[i - 1][j];
+                }
+            }
         }
-      }
     }
-  }
-  clock_t stopCPUTime = clock();
-  auto stopWallTime = chrono::system_clock::now();
-  elapsedCPUTime = (stopCPUTime - startCPUTime) / (double)CLOCKS_PER_SEC;
-  chrono::duration<double> WallTime = (stopWallTime - startWallTime);
-  elapsedWallTime = WallTime.count();
-  std::cout << "Optimal: " << table[numberOfItems][maximumCapacity]
-            << std::endl;
-  std::cout << "Finished in " << std::endl;
-  std::cout << "\t- " << elapsedCPUTime << " seconds [CPU Clock]" << std::endl;
-  std::cout << "\t- " << elapsedWallTime << " seconds [Wall Clock]"
-            << std::endl;
+    clock_t stopCPUTime = clock();
+    auto stopWallTime = chrono::system_clock::now();
+    elapsedCPUTime = (stopCPUTime - startCPUTime) / (double)CLOCKS_PER_SEC;
+    chrono::duration<double> WallTime = (stopWallTime - startWallTime);
+    elapsedWallTime = WallTime.count();
+    std::cout << "Optimal: " << table[numberOfItems][maximumCapacity]
+              << std::endl;
+    std::cout << "Finished in " << std::endl;
+    std::cout << "\t- " << elapsedCPUTime << " seconds [CPU Clock]" << std::endl;
+    std::cout << "\t- " << elapsedWallTime << " seconds [Wall Clock]"
+              << std::endl;
 
-  if (instanceFile != INST_NONE) {
-    writeResults();
-  }
-  return {table[numberOfItems][maximumCapacity], elapsedWallTime};
+    if (instanceFile != INST_NONE) {
+        writeResults();
+    }
+    return {table[numberOfItems][maximumCapacity], elapsedWallTime};
 }
 
 void KnapsackDP::writeResults() {
-  // Guardamos los resultados en un fichero
-  ofstream outputFile(instanceFile);
-  outputFile << table[numberOfItems][maximumCapacity] << endl
-             << elapsedWallTime;
-  outputFile.close();
+    // Guardamos los resultados en un fichero
+    ofstream outputFile(instanceFile);
+    outputFile << table[numberOfItems][maximumCapacity] << endl
+               << elapsedWallTime;
+    outputFile.close();
 }
--- src/knapsackDP.h
+++ src/knapsackDP.h
@@ -22,39 +22,41 @@ using w = int;
 using results = std::pair<optimal, elapsedTime>;
 
 class KnapsackDP {
- public:
-  KnapsackDP(const string& filename, const fs::path& pathToResults);
+   public:
+    KnapsackDP(const string& filename, const fs::path& pathToResults);
 
-  KnapsackDP(const int& nItems, vector<p>& profits, vector<w>& weights,
-             const p& capacity);
+    KnapsackDP(const int& nItems, vector<p>& profits, vector<w>& weights,
+               const p& capacity);
 
-  virtual ~KnapsackDP();
+    virtual ~KnapsackDP();
 
-  results run();
+    results run();
 
- private:
-  // Maximum weight the knapsack can carry
-  p maximumCapacity;
-  // Number of items to put in the KP
-  int numberOfItems;
-  // Weights and profits of each item
-  vector<w> weights;
-  vector<p> profits;
-  // Elapsed time of the algorithm
-  double elapsedCPUTime;
-  double elapsedWallTime;
-  // File which defines the instance
-  string instanceFile;
-  // Variables of the DP algorithm
-  vector<vector<p>> table;
+   private:
+    // Maximum weight the knapsack can carry
+    p maximumCapacity;
+    // Number of items to put in the KP
+    int numberOfItems;
+    // Weights and profits of each item
+    vector<w> weights;
+    vector<p> profits;
+    // Elapsed time of the algorithm
+    double elapsedCPUTime;
+    double elapsedWallTime;
+    // File which defines the instance
+    string instanceFile;
+    // File where the results are saved
+    string outputFile;
+    // Variables of the DP algorithm
+    vector<vector<p>> table;
 
- private:
-  static const string INST_NONE;
-  static const char ESCAPE;
-  static const string EXTENSION;
+   private:
+    static const string INST_NONE;
+    static const char ESCAPE;
+    static const string EXTENSION;
 
- private:
-  void writeResults();
+   private:
+    void writeResults();
 };
 
 #endif
